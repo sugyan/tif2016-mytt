1e8d279be162eb9d155c9f3f42bd59598fbd35f5
sugyan <sugi1982@gmail.com>
2016-07-26 22:00:41 +0900

    bebel-loader, fetch

--- app/assets/src/js/main.js
+++ app/assets/src/js/main.js
@@ -1,1 +1,11 @@
-console.log('foo');
+import 'whatwg-fetch';
+
+fetch('/api/timetable.json').then((response) => {
+    return response.json();
+}).then((json) => {
+    json.forEach((e) => {
+        window.console.log(JSON.stringify(e));
+    });
+}).catch((err) => {
+    window.console.error(err);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
+/* eslint-env node */
+
 module.exports = {
     entry: './app/assets/src/js/main.js',
     output: {
         path: './public/javascripts',
         publicPath: '/javascripts/',
         filename: 'application.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
     },
     devServer: {
         inline: true
