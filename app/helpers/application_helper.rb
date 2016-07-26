35e6fbe66691eb3ef60ec4da34c1e3293d85f043
sugyan <sugi1982@gmail.com>
2016-07-26 20:14:51 +0900

    setup for webpack-dev-server

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def javascript_include_tag(*sources)
+    if Rails.env.development?
+      opts = {
+        src: '//localhost:8080/javascripts/application.js'
+      }
+      return content_tag(:script, '', opts)
+    end
+    super(*sources)
+  end
 end
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ module.exports = {
     entry: './app/assets/src/js/main.js',
     output: {
         path: './public/javascripts',
+        publicPath: '/javascripts/',
         filename: 'application.js'
+    },
+    devServer: {
+        inline: true
     }
 };
