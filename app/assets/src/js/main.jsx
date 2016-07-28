43cbfddea5f7ca2372ecf70ea572fbe1180abbde
sugyan <sugi1982@gmail.com>
2016-07-28 23:59:08 +0900

    add moment

--- app/assets/src/js/main.jsx
+++ app/assets/src/js/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment';
 import 'whatwg-fetch';
 
 export default class App extends React.Component {
     constructor(props) {
         super(props);
+        this.days = {
+            day1: '08-05',
+            day2: '08-06',
+            day3: '08-07'
+        };
         this.state = {
             stages: []
         };
@@ -22,9 +28,11 @@ export default class App extends React.Component {
     }
     render() {
         const stages = this.state.stages.map((e, i) => {
+            const start = moment(`2016-${this.days[e.day]} ${e.start}+09:00`, 'YYYY-MM-DD HHmmZ');
+            const end   = moment(`2016-${this.days[e.day]} ${e.end  }+09:00`, 'YYYY-MM-DD HHmmZ');
             return (
                 <div key={i}>
-                  {`${e.day}: ${e.start} - ${e.end} [${e.stage}] ${e.artist}`}
+                  {`${start.format('M/D (ddd)')}: ${start.format('HH:mm')} - ${end.format('HH:mm')} [${e.stage}] ${e.artist}`}
                 </div>
             );
         });
